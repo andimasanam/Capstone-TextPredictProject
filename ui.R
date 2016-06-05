3d047bb29da9a863a2cdee7049beb8108ee8acf0
Andi M Masanam <andimarimuthu@gmail.com>
2016-06-06 00:28:57 +0530

    Update ui.R

--- ui.R
+++ ui.R
@@ -1,26 +1,18 @@
-library('shiny')
-library('shinyjs')
-
-# Define UI for application that draws a histogram
-ui = fluidPage(
-  shinyjs::useShinyjs(),
-  shinyjs::inlineCSS(list(.big = "font-size: 2em")),
-  div(id = "myapp",
-      h2("Capstone Data Science Text Prediction Project"),
-      textInput("Input", "Text Input :", ""),
-      p('Type up to 2 words in sequence with space as system will auto detect the word(s) and suggest you the best predictive next word in table format.'),
-      p("Timestamp: ",
-        span(id = "time", date()),
-        a(id = "update", "Update", href = "#")
-      ),
-      actionButton("reset", "Reset form")
-  ),
-  mainPanel(
-    verbatimTextOutput('text1'),
-    verbatimTextOutput('text2'),
-    br(),
-    div(id = "Predict", p('The best match next predictive word is :', htmlOutput('text3'))),
+library(shiny)
+shinyUI(fluidPage(
+    titlePanel("Coursera Capstone Text Prediction Application"),
+    fluidRow("Please give 10-15 seconds for the app to load in the background."),
     hr(),
-    DT::dataTableOutput('table')
-  )
+    fluidRow("In the space"),
+    fluidRow("1. Type a sentence"),
+    fluidRow("2. Press Submit"), 
+    fluidRow("Wait for few seconds... and then you will see a few words that are likely to come next!"),
+    hr(),
+    textInput('sentenceInputVar', 'Input Sentence here:'),
+    actionButton('submitButton', 'Submit'),
+    fluidRow('(Example: "I want to go" or "She used to")'),
+    verbatimTextOutput("oWordPredictions"),
+    
+    hr(),
+)
 )
